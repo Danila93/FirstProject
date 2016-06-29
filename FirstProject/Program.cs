37c25bfea5e50468a0ecf5b03c54630d17441aba
Данила Ермошин <ermoshin.danila@yandex.ru>
2016-06-29 12:46:55 +0300

    Добавьте файлы проекта.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("input a: ");
+            a = Convert.ToInt16(Console.ReadLine());
+        }
+    }
+}
